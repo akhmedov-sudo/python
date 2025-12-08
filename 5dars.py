10ba55496d3bb192853f2f21eb72c7588ede1a28
akhmedov-sudo <xsatmrich@gmail.com>
2025-12-08 13:04:29 +0300

    Add files via upload

---
+++ 5dars.py
@@ -0,0 +1,96 @@
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# cars.append("Audi")
+# print(cars)  # Output: ['BMW', 'Mercedes', 'Toyota', 'Honda', 'Audi']
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# cars.insert(2, "Audi")
+# print(cars)  # Output: ['BMW', 'Mercedes', 'Audi', 'Toyota', 'Honda']
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# cars.remove("Toyota")
+# print(cars)
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# mers = cars.pop(0)
+# print(car)
+# print(cars)  # Output: ['Mercedes', 'Toyota', 'Honda']  # Output: ['BMW', 'Mercedes', 'Honda']
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# del cars[1]
+# print(cars)  # Output: ['BMW', 'Toyota', 'Honda']
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# cars.clear()
+# print(cars)  # Output: []
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# cars.sort()
+# print(cars)  # Output: ['BMW', 'Honda', 'Mercedes', 'Toyota']
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# cars.sort(reverse=True)
+# print(cars)  # Output: ['Toyota', 'Mercedes', 'Honda', 'BMW']
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# sorted_cars = sorted(cars)
+# print(sorted_cars)  # Output: ['BMW', 'Honda', 'Mercedes', 'Toyota']
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# # cars.reverse()
+# # print(cars)  # Output: ['Honda', 'Toyota', 'Mercedes', 'BMW']
+# print(len(cars))
+# sonlar=list(range(0,20))
+# print(sonlar)  # Output: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+# sonlar = list(range(0, 20, 2))
+# print(sonlar)  # Output: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+# sonlar = list(range(0, 20, 3))
+# katta = max(sonlar) -> katta sonni topish
+# kichik = min(sonlar) -> kichik sonni topish
+# jami = sum(sonlar) -> sonlar yig'indisini topish
+# print(sonlar)  # Output: [0, 3, 6, 9, 12, 15, 18]
+# print(katta)  # Output: 18
+# print(kichik)  # Output: 0
+# print(jami)  # Output: 63
+
+# ----------------------------------------------------------------------------------------------------------------
+
+# davlatlar = ["O'zbekiston", "AQSH", "Rossiya", "Qozog'iston"]
+# print(davlatlar[0], davlatlar[1], davlatlar[2], davlatlar[3]) # Output: O'zbekiston AQSH Rossiya Qozog'iston
+# mevalar = ["olma", "anor", "uzum", "anjir"]
+# print(len(mevalar))  # Output: 4
+
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# sorted_cars = sorted(cars)
+# print(sorted_cars)  # Output: ['BMW', 'Honda', 'Mercedes', 'Toyota']
+
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# sorted_cars = sorted(cars, reverse=True)
+# print(sorted_cars)  # Output: ['Toyota', 'Mercedes', 'Honda', 'BMW']
+
+# cars = ["BMW", "Mercedes", "Toyota", "Honda"]
+# sorted_cars = sorted(cars)
+# print(sorted_cars)  # Output: ['BMW', 'Honda', 'Mercedes', 'Toyota']
+# print(cars)  # Output: ['BMW', 'Honda', 'Mercedes', 'Toyota']``
+
+# shops = ["Supermarket", "Electronics Store", "Clothing Store", "Bookstore"]
+# shops.reverse()
+# print(shops)  # Output: ['Bookstore', 'Clothing Store', 'Electronics Store', 'Supermarket']
+
+# sonlar = list(range(120, 1200, 2))
+# print(sonlar)  # Output: [120, 122, 124, ..., 1198]
+
+# sonlar = [120, 122, 124, 126, 128, 130, 132, 134, 136, 138,]
+# jami = sum(sonlar)
+# print(jami)  # Output: 1290
+
+# sonlar = [120, 122, 124, 126, 128, 130, 132, 134, 136, 138]
+# katta = max(sonlar)
+# kichik = min(sonlar)
+# ayirma = katta - kichik
+# print(ayirma)  # Output: 18
+# print(katta)  # Output: 138
+# print(kichik)  # Output: 120
+
+# sonlar = list(range(120, 1200))
+# hisoblang = sum(sonlar)
+# print(hisoblang)  # Output: 713580
+
+# cars = ["BMW", "Mercedes", "Toyota", "Honda", "Audi", "Kia", "Hyundai"]
+# inomarka=cars[0:4]
+# print(inomarka)  # Output: ['BMW', 'Mercedes', 'Toyota', 'Honda', 'Audi']
+# toys = ["Lego", "Action Figure", "Doll", "Puzzle", "Board Game", "RC Car", "Stuffed Animal"]
+# toys_list=toys
+# toys.append("Yo-yo")
+# tuple_toys=tuple(toys_list)
+# print(tuple_toys)  # Output: ('Doll', 'Puzzle', 'Board Game')   
