1fcf5239d6b95a3e46951898bfeadf248184f3a8
akhmedov-sudo <xsatmrich@gmail.com>
2025-12-10 15:30:41 +0500

    Add files via upload

---
+++ 6dars.py
@@ -0,0 +1,207 @@
+# yosh = 14
+# if yosh<=15:
+#     print("Sizga kirish bepul")
+# else:
+#     print("Sizdan kirish uchun 10000 so'm olinadi")
+
+# yosh = int(input("Yoshingizni kiriting: "))
+# if yosh<=15:
+#     print("Sizga kirish bepul")
+# else:
+#     print("Sizdan kirish uchun 10000 so'm olinadi")
+# yosh = int(input("Yoshingizni kiriting: "))
+# if yosh<=4 or yosh>=60:
+#     print("Sizga kirish bepul")
+# elif 5<=yosh<=15:
+#     print("Sizdan kirish uchun 5000 so'm olinadi")
+# else:
+#     print("Sizdan kirish uchun 10000 so'm olinadi")
+
+# ball = int(input("Balingizni kiriting: "))
+# if ball>=90:
+#     print("Alo")
+# elif 80<=ball<90:
+#     print("Yaxshi")
+# elif 60<=ball<80:
+#     print("Qoniqarli")
+# else:
+#     print("Qaytadan urinib ko'ring")
+
+# ball = 75
+# if ball>=90:
+#     print("Alo")
+# elif 80<=ball<90:
+#     print("Yaxshi")
+# elif 60<=ball<80:
+#     print("Qoniqarli")
+# else:
+#     print("Qaytadan urinib ko'ring")
+# car = input("Qaysi mashinani xohlaysiz? ")
+# if car.lower()=='tesla':
+#     print("Tesla ajoyib tanlov!")
+# else:
+#     print(f"{car.title()} ham yaxshi mashina")
+# a,b,c = 5,-10,-1
+# if a>0 and b>0 and c>0:
+#     print("Barcha sonlar musbat")
+# elif (a>0 or b>0 or c>0) or (a>0 or b<0 or c>0) or (a>0 and b>0 and c>0):
+#     print("Sonlardan kamida bittasi manfiy")
+# elif (a>0 or b>0 or c<0) or (a>0 or b<0 or c>0) or (a<0 and b>0 and c>0):
+#     print("Sonlardan kamida bittasi manfiy")
+# else:
+#     print("Barcha sonlar musbat")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# if a>0 and b>0 and c>0:
+#      print("Barcha sonlar musbat")
+# elif (a>0 or b>0 or c>0) or (a>0 or b<0 or c>0) or (a>0 and b>0 and c>0):
+#      print("Sonlardan kamida bittasi manfiy")
+# elif (a>0 or b>0 or c<0) or (a>0 or b<0 or c>0) or (a<0 and b>0 and c>0):
+#      print("Sonlardan kamida bittasi manfiy")
+# else:
+#      print("Barcha sonlar musbat yoq")
+
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# if a>0 and b>0 and c>0:
+#      print("Barcha sonlar musbat")
+# elif a<0 and b<0 and c<0:
+#         print("Barcha sonlar manfiy")
+# else:
+#      print("Sonlardan kamida bittasi manfiy va musbat")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# if a>b:
+#         print(f"{a} katta son")
+# elif b>a:
+#         print(f"{b} katta son")
+# else:
+#         print("Sonlar teng")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# if a<b:
+#         print(f"{a} kichik son")
+# elif b<a:
+#         print(f"{b} kichik son")
+# else:
+#         print("Sonlar teng")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# if a<b:
+#         print(f"{a} katta son")
+# elif b<a:
+#         print(f"{b} kichik son")    
+# else:
+#         print(f"{b} lichik son")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# if a<b and a<c:
+#         print(f"{a} kichik son")
+# elif b>a and b>c:
+#         print(f"{b} kichik son")
+# else:
+#         print(f"{c} kichik son")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# if a>b and a>c:
+#  print(f"{a} katta son")
+# elif b>a and b>c:
+#  print(f"{b} katta son")
+# else:
+#  print(f"{c} katta son")
+# if a<b and a<c:
+#  print(f"{a} kichik son")
+# elif b<a and b<c:
+#  print(f"{b} kichik son")
+# else:
+#  print(f"{c} kichik son")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# if a<b and a<c:
+#  print(f"{a} kichik son")
+# elif b<a and b<c:
+#     print(f"{b} kichik son")
+# else:
+#     print(f"{c} kichik son")
+# if a>b and a>c:
+#  print(f"{a} katta son")
+# elif b>a and b>c:
+#     print(f"{b} katta son")
+# else:
+#     print(f"{c} katta son")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# if a<b and a<c:
+#  print(f"{a} kichik son")
+# elif b<a and b<c:
+#  print(f"{b} kichik son")
+# else:
+#  print(f"{c} kichik son")
+# if a>b and a>c:
+#     print(f"{a} katta son")
+# elif b>a and b>c:
+#     print(f"{b} katta son")
+# else:
+#     print(f"{c} katta son")
+# sum = a + b + c
+# print(f"Sonlar yig'indisi: {sum}")
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# c = int(input("Uchinchi sonni kiriting: "))
+# if a>b and a>c:
+#     print(f"{a} katta son")
+# elif b>a and b>c:
+#     print(f"{b} katta son")
+# else:
+#     print(f"{c} katta son")
+# if a<b and a<c:
+#     print(f"{a} kichik son")
+# elif b<a and b<c:
+#     print(f"{b} kichik son")
+# else:
+#     print(f"{c} kichik son")
+
+# sonlar = [a, b, c]
+# sonlar.sort(reverse=True)
+# print(f"Eng katta ikki son: {sonlar[0]} va {sonlar[1]}")
+# print(f"Sonlar yig'indisi: {a + b + c}")
+
+
+# a,b,c = -5,10,15
+# abc = a*2,b*2,c*2
+# if a>0 and b>0 and c>0:
+#     print("Barcha sonlar musbat")
+#     print(abc)
+# else:
+#     print("Sonlardan kamida bittasi manfiy")
+#     print(abc)
+# abc = a/2,b/2,c/2
+# a,b,c = 2,-8,45
+# abc = a*2,b*2,c*2
+# if a>0 and b>0 and c>0:
+#     print("Barcha sonlar musbat")
+#     print(abc)
+# else:
+#     print("Sonlardan kamida bittasi manfiy")
+#     print(abc)
+# abc = a/2,b/2,c/2
+# a,b,c=4,12,12
+# if a>b and b>c:
+#     print("teng emas")
+# else:
+#         print("teng")
+# a,b,c,d = 14,10,10,20 # a,b,c teng, d teng emas
+# if a==b and b==c and c!=d:
+#     print(f"a, {a} b, {b} c, {c} sonlari teng, lekin bitta soni ularga teng emas. = {d}")
+# else:
+#     print("Shart bajarilmadi: uchta son teng emas")
+a,c,b = 10,5,50
+x = a-c
+v = a-b
+print(f"a va c masofasi {x} eng yaqin",f"\na va b masofasi {v} eng uzoqi ")
