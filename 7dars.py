150b446a53eb5000de78d1d719eb0335df209c5a
akhmedov-sudo <xsatmrich@gmail.com>
2025-12-17 15:31:20 +0500

    Add files via upload

---
+++ 7dars.py
@@ -0,0 +1,127 @@
+# juft = int(input("Juft son kiriting: "))
+# if juft % 2 == 0:
+#     print("Rahmat!")
+# else:
+#     print("Bu son juft emas.")
+
+# --------------------------------------
+
+# muzey = int(input("nechi yoshga kirgansiz >>>> "))
+# # if muzey < 4 and muzey > 60:
+# #     print("muzeyga kirish bepul")
+# # elif muzey < 18:
+# #     print("muzeyga kirish narxi 10000 so'm")
+# # elif muzey > 18:
+# #     print("muzeyga kirish narxi 20000 so'm")
+
+# --------------------------------------
+
+# x = float(input("Birinchi sonni kiriting: "))
+# y = float(input("Ikkinchi sonni kiriting: "))
+# if x==y:
+#     print(f"{x}={y}")
+# elif x<y:
+#     print(f"{x}<{y}")
+# else:
+#     print(f"{x}>{y}")
+
+# --------------------------------------
+
+# mahsulotlar = ["un", "yog'", "sovun", "tuxum", "piyoz", "kartoshka", "olma", "banan", "uzum", "qovun"]
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         print(f"Do'konimizda {mahsulot} bor")
+#     else:
+#         print(f"Do'konimizda {mahsulot} yo'q")
+
+
+# --------------------------------------
+
+# mahsulotlar = ["un", "yog'", "sovun", "tuxum", "piyoz", "kartoshka", "olma", "banan", "uzum", "qovun"]
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+#     yoq_mahsulotlar = []
+# for mahsulot in savat:
+#     if mahsulot not in mahsulotlar:
+#         yoq_mahsulotlar.append(mahsulot)
+# if yoq_mahsulotlar:
+#     print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#     for mahsulot in yoq_mahsulotlar:
+#         print(mahsulot)
+
+# --------------------------------------
+
+# login = input("Loginni tanlang: ")
+# if login == "abror":
+#     print("Xush kelibsiz, Abror!")
+# else:
+#     print("login xato yoki band etilgan")
+
+# --------------------------------------
+
+
+
+# son = int(input("Istalgan butun son kiriting: "))
+
+# for n in range(2,11):
+#     if not (son%n):
+#         print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
+
+# ism = ["ali", "vali", "dali", "javohir"]
+# for ismlar in ism:
+#     print(f"Salom {ismlar}")
+# print("bu kod 5 marta qaytarildi\n")
+
+# sonlar = list(range(11,100,2))
+# for son in sonlar:
+#     print(son**3)
+
+# for a in range(1,11,2):  ---->>> toq va juftni chqarish
+#     print(a)
+
+# for i in range(1,11):
+#     print(i)
+
+# mevalar = ["uzum ", "banan", "olma"]
+# for meva in mevalar:
+#     print(meva)
+
+# soz = "python"
+# for harf in soz:
+#     print(harf)
+
+# k = 5
+# n = 8
+# for i in range(n):
+#     print(k)
+
+# k = int(input("1 soni kiriting: "))
+# n = int(input("2 soni kiriting: "))
+# for i in range(n):
+#     print(k)
+
+# a = 1
+# b = 55
+# for i in range(a,b):
+#     print(i)
+
+# a = 45
+# b = 12
+# for i in range(a,b,-1):
+#     print(i)
+
+# kofe_narxi = float(input("1 kg kofening narxini kiriting: "))
+# for kg in range(1, 11):
+#     print(f"{kg} kg kofening narxi: {kg * kofe_narxi} so'm")
+
+# kofe_narxi = float(input("1 kg kofening narxini kiriting: "))
+# for kg in range(1, 11):
+#     print(f"{kg/10} kg kofening narxi: {kg * kofe_narxi} so'm")
+
+
+
+    
