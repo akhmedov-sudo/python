4e9be6f511b59752c04e93f314b5c924db3bd759
akhmedov-sudo <xsatmrich@gmail.com>
2025-12-22 15:20:29 +0500

    Add files via upload

---
+++ 8dars.py
@@ -0,0 +1,21 @@
+# phone = {"model":"samsung a50", "yili": 2020, "Xotirasi": "128gb", "rangi": "Qora"}
+# print(phone)
+# phone["Nrxi"]=200
+# print(phone)
+# phone["Kamera"]="48mp"
+# print(phone)
+# oquvchi = {"ismi":"Adhamjon", "manzil":"Bogdod", "yoshi":15, "maktab":"1-maktab"}
+# # print(f"{oquvchi['ismi']},{oquvchi['manzil']},{oquvchi['yoshi']},{oquvchi['maktab']}")
+# # print(f"Mening telfonim {phone['model']}, U {phone['yili']}-yil ishlab chiqarilgan , Xotirasi {phone['Xotirasi']}, Rangim {phone['rangi']}")
+# print(f"Mening ismim {oquvchi['ismi']}, Manzilim {oquvchi['manzil']}, Yoshim {oquvchi['yoshi']}, Maktabim {oquvchi['maktab']}")
+# phone = {"model":"iphone 17", "Rangi":"orange", "xotirasi":"128gb", "yil":2023}
+# print(phone)
+# del phone["Rangi"]
+# print(phone)
+# oila = {"onam":"feruzaxon", "akam":"polat", "ukam":"asadbek", "buvam":"anorxon", "yili":1989, "yili2":2016, "yili3":1954}
+# print(f"onamini ismi {oila['onam']} , {oila["yili"]}-yilda, Bogdod viloyatida tugilgan ")
+# print(f"Buvamni ismi {oila['buvam']} , {oila["yili3"]}-yilda, Bogdod viloyatida tugilgan ")
+# print(f"Ukamni ismi ismi {oila['ukam']} , {oila["yili"]}-yilda, Bogdod viloyatida tugilgan ")
+
+oila = {"ukam1":"ali", "ukam2":"asadbek", "ukam3":"Akobir", "akam1":"polat", "akam2":"diyorbek"}
+print(f"{oila["ukam1"]}ning sevimli ovqati osh,\n {oila["ukam2"]}ning sevimli ovqati lavash,\n {oila["ukam3"]}ning sevimli ovqati sho'rva,\n {oila["akam1"]}ning sevimli ovqati hoddog,\n {oila["akam2"]}ning sevimli ovqati makaron")
